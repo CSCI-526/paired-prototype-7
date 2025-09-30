bb99874f9e1873b42c298b3e55478f8f3b8f2e37
Harsh Jain <jain.hn@somaiya.edu>
2025-09-30 16:08:04 -0700

    Feature: Removed shadows in multiplayer and updated controls and camera vierws for horizontal split screens.

--- Assets/Scripts/Player1Controller.cs
+++ Assets/Scripts/Player1Controller.cs
@@ -1,14 +1,55 @@
+using TMPro;
 using UnityEngine;
-public class Player1Controller : MonoBehaviour {
-    public float speed = 10f;
-    void Update() {
-        float h = 0, v = 0;
-        if (Input.GetKey(KeyCode.W)) v += 1;
-        if (Input.GetKey(KeyCode.S)) v -= 1;
-        if (Input.GetKey(KeyCode.A)) h -= 1;
-        if (Input.GetKey(KeyCode.D)) h += 1;
 
-        Vector3 movement = new Vector3(h, 0, v).normalized * speed * Time.deltaTime;
-        transform.Translate(movement);
+[RequireComponent(typeof(Rigidbody))]
+public class Player1Controller : MonoBehaviour
+{
+    public GameObject speed;
+    public float motorPower = 800f;
+    public float steerTorque = 200f;
+    public float maxSpeed = 40f;
+    public float brakeDrag = 2f;
+    public float normalDrag = 0.1f;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = new Vector3(0f, -0.5f, 0f);
+        rb.linearDamping = normalDrag;
+    }
+
+    void FixedUpdate()
+    {
+        float accel = 0f;
+        if (Input.GetKey(KeyCode.W)) accel = 1f;
+        else if (Input.GetKey(KeyCode.S)) accel = -1f;
+
+        float steer = 0f;
+        if (Input.GetKey(KeyCode.D)) steer = 1f;
+        else if (Input.GetKey(KeyCode.A)) steer = -1f;
+
+        bool braking = Input.GetKey(KeyCode.Space);
+
+        Vector3 forward = transform.forward;
+        float forwardVel = Vector3.Dot(rb.linearVelocity, forward);
+
+        if (!(accel > 0f && forwardVel > maxSpeed))
+        {
+            rb.AddForce(forward * accel * motorPower * Time.fixedDeltaTime, ForceMode.Acceleration);
+        }
+
+        float steerFactor = 10f;
+        rb.AddRelativeTorque(Vector3.up * steer * steerTorque * steerFactor * Time.fixedDeltaTime, ForceMode.Acceleration);
+
+        Vector3 right = transform.right;
+        float lateralVel = Vector3.Dot(rb.linearVelocity, right);
+        Vector3 lateralImpulse = -right * lateralVel * 0.8f;
+        rb.AddForce(lateralImpulse, ForceMode.VelocityChange);
+
+        rb.linearDamping = braking ? brakeDrag : normalDrag;
+
+        speed.GetComponent<TMP_Text>().text = $"{Mathf.RoundToInt(forwardVel * 2.237f)}";
     }
 }
--- Assets/Scripts/Player2Controller.cs
+++ Assets/Scripts/Player2Controller.cs
@@ -1,14 +1,55 @@
+using TMPro;
 using UnityEngine;
-public class Player2Controller : MonoBehaviour {
-    public float speed = 10f;
-    void Update() {
-        float h = 0, v = 0;
-        if (Input.GetKey(KeyCode.UpArrow)) v += 1;
-        if (Input.GetKey(KeyCode.DownArrow)) v -= 1;
-        if (Input.GetKey(KeyCode.LeftArrow)) h -= 1;
-        if (Input.GetKey(KeyCode.RightArrow)) h += 1;
 
-        Vector3 movement = new Vector3(h, 0, v).normalized * speed * Time.deltaTime;
-        transform.Translate(movement);
+[RequireComponent(typeof(Rigidbody))]
+public class Player2Controller : MonoBehaviour
+{
+    public GameObject speed;
+    public float motorPower = 800f;
+    public float steerTorque = 200f;
+    public float maxSpeed = 40f;
+    public float brakeDrag = 2f;
+    public float normalDrag = 0.1f;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = new Vector3(0f, -0.5f, 0f);
+        rb.linearDamping = normalDrag;
+    }
+
+    void FixedUpdate()
+    {
+        float accel = 0f;
+        if (Input.GetKey(KeyCode.UpArrow)) accel = 1f;
+        else if (Input.GetKey(KeyCode.DownArrow)) accel = -1f;
+
+        float steer = 0f;
+        if (Input.GetKey(KeyCode.RightArrow)) steer = 1f;
+        else if (Input.GetKey(KeyCode.LeftArrow)) steer = -1f;
+
+        bool braking = Input.GetKey(KeyCode.RightControl) || Input.GetKey(KeyCode.LeftControl);
+
+        Vector3 forward = transform.forward;
+        float forwardVel = Vector3.Dot(rb.linearVelocity, forward);
+
+        if (!(accel > 0f && forwardVel > maxSpeed))
+        {
+            rb.AddForce(forward * accel * motorPower * Time.fixedDeltaTime, ForceMode.Acceleration);
+        }
+
+        float steerFactor = 10f;
+        rb.AddRelativeTorque(Vector3.up * steer * steerTorque * steerFactor * Time.fixedDeltaTime, ForceMode.Acceleration);
+
+        Vector3 right = transform.right;
+        float lateralVel = Vector3.Dot(rb.linearVelocity, right);
+        Vector3 lateralImpulse = -right * lateralVel * 0.8f;
+        rb.AddForce(lateralImpulse, ForceMode.VelocityChange);
+
+        rb.linearDamping = braking ? brakeDrag : normalDrag;
+
+        speed.GetComponent<TMP_Text>().text = $"{Mathf.RoundToInt(forwardVel * 2.237f)}";
     }
 }
